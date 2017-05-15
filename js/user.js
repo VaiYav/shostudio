bed5b8b89933e17f04c045e655ba67316cf32a64
VaiYav <yakovlev.valentin97@gmail.com>
2017-05-15 18:01:02 +0300

    first commit

---
+++ js/user.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+			direction: 'vertical',
+			loop: true,
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			direction: 'vertical',
+			autoplay: 2500
+		})
